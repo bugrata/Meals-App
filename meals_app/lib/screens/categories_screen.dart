f2fa485a8a00e6327f7b99b0a93bf3b4e3c08e80
Buğrahan <bugraata2344@gmail.com>
2023-10-12 21:31:37 +0300

    General Update

--- meals_app/lib/screens/categories_screen.dart
+++ meals_app/lib/screens/categories_screen.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/category.dart';
+import 'package:meals_app/screens/meals_screen.dart';
+import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
+    Navigator.of(context).push(
+      MaterialPageRoute(
+          builder: (ctx) =>
+              MealsScreen(meals: filteredMeals, title: category.title)),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,61 +26,21 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('Select Your Category'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
         ),
-        children: const [
-          Text(
-            '1',
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
-          Text(
-            '1',
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
-          Text(
-            '1',
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
-          Text(
-            '1',
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
-          Text(
-            '1',
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
-          Text(
-            '1',
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
-          Text(
-            '1',
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
-          Text(
-            '1',
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
+            )
         ],
       ),
     );
